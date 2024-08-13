4d48222117fa25e64c2021a8bd94402733371d16
Matheus Magalhaes <magalhaes.almeida.matheus@gmail.com>
2024-08-13 20:16:17 -0300

    adiciona classe de configuração do perfil test e criação implementação de repository

---
+++ src/main/java/com/devmatheus/vendas/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.devmatheus.vendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devmatheus.vendas.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
