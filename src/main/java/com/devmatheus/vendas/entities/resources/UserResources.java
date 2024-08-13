d2270fe7f971638cef928c6af8eee2ae86e23d5c
Matheus Magalhaes <magalhaes.almeida.matheus@gmail.com>
2024-08-13 20:02:57 -0300

    Adiciona resource e entidade user e configurações de banco de dados h2 e jpa

---
+++ src/main/java/com/devmatheus/vendas/entities/resources/UserResources.java
@@ -0,0 +1,19 @@
+package com.devmatheus.vendas.entities.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devmatheus.vendas.entities.User;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserResources {
+
+  @GetMapping
+  ResponseEntity<User> findAll() {
+    User u = new User(1l, "Matheus", "matheus@gmail.com", "999999", "12345");
+    return ResponseEntity.ok().body(u);
+  }
+}
