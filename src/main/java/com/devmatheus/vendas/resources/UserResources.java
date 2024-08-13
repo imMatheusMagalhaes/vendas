6839adece1107ff820008f5955f3e0b63a4b5967
Matheus Magalhaes <magalhaes.almeida.matheus@gmail.com>
2024-08-13 20:32:39 -0300

    adiciona service de user e implementa find all e find by id

--- src/main/java/com/devmatheus/vendas/resources/UserResources.java
+++ src/main/java/com/devmatheus/vendas/resources/UserResources.java
@@ -1,19 +1,34 @@
 package com.devmatheus.vendas.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devmatheus.vendas.entities.User;
+import com.devmatheus.vendas.services.UserService;
 
 @RestController
 @RequestMapping(value = "/user")
 public class UserResources {
 
+  @Autowired
+  UserService service;
+
   @GetMapping
-  ResponseEntity<User> findAll() {
-    User u = new User(1l, "Matheus", "matheus@gmail.com", "999999", "12345");
-    return ResponseEntity.ok().body(u);
+  ResponseEntity<List<User>> findAll() {
+    List<User> users = service.findAll();
+    return ResponseEntity.ok().body(users);
   }
+
+  @GetMapping(value = "/{id}")
+  ResponseEntity<User> findById(@PathVariable Long id) {
+    User user = service.findById(id);
+    return ResponseEntity.ok().body(user);
+  }
+
 }
